06653a5e8418bdf0d36d96561883407496cb100a
Frode Thorsen Børseth <frodetb@gmail.com>
2019-11-18 01:23:02 +0100

    Started writing a few tests

--- fluid_sim.py
+++ fluid_sim.py
@@ -1,25 +1,7 @@
 import copy
 
 import numpy as np
-
-if __name__ != "__main__":
-    N = 50
-    N_t = 10
-    h = 0.1
-    mask = Mask([0.0] * (N * N))
-
-    mu = 1.0
-    zeta = 1.0
-    kappa = zeta + (mu / 3)
-    p = 0  # TODO
-
-    u0 = mask.apply_mask(np.zeros(N * N))
-    v0 = mask.apply_mask(np.zeros(N * N))
-    rho0 = mask.apply_mask(copy.deepcopy(p))  # TODO
-
-    x0 = TripleVector(u0, v0, rho0)
-
-    solution = solver_example(N, h, N_t, mu, kappa, x0, p, mask)
+import pytest
 
 
 def RK4(h, x, f):
@@ -27,19 +9,7 @@ def RK4(h, x, f):
     k2 = h * f(x + 0.5 * k1)
     k3 = h * f(x + 0.5 * k2)
     k4 = h * f(x + k3)
-    return x + (k1 + 2*k2 + 2*k3 + k4) / 6
-
-
-def solver_example(N, h, N_t, mu, kappa, x0, p, mask):
-    f = TimeDerivativeTriple(
-        TimeDerivativeU(N, mu, kappa, p, mask, edge_bc="DIRICHLET", interior_bc="NEUMANN"),
-        TimeDerivativeV(N, mu, kappa, p, mask, edge_bc="DIRICHLET", interior_bc="NEUMANN"),
-        TimeDerivativeRho(N, mu, kappa, p, mask, edge_bc="NEUMANN", interior_bc="DIRICHLET"),
-    )
-    states = [x0]
-    for n_t in range(N_t):
-        states.append(RK4(h, states[-1], f))
-    return states
+    return x + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 
 class IllegalMultiplicationException(Exception):
@@ -51,6 +21,9 @@ class TripleVector:
         self.u = u
         self.v = v
         self.rho = rho
+    
+    def __neg__(self,):
+        return TripleVector(-self.u, -self.v, -self.rho)
 
     def __add__(self, other):
         return TripleVector(self.u + other.u, self.v + other.v, self.rho + other.rho,)
@@ -62,47 +35,93 @@ class TripleVector:
         if not type(other) in [int, float]:
             raise IllegalMultiplicationException()
         return TripleVector(self.u * other, self.v * other, self.rho * other,)
+    
+    def __truediv__(self, other):
+        if not type(other) in [int, float]:
+            raise IllegalDivisionException()
+        return TripleVector(
+            self.u.__truediv__(other), 
+            self.v.__truediv__(other), 
+            self.rho.__truediv__(other),
+        )
 
     def __rmul__(self, other):
         return self * other
+    
+    def __str__(self,):
+        return "<u: {}, v: {}, rho: {}>".format(self.u, self.v, self.rho)
+    
+    def __repr__(self,):
+        return "<TripleVector({}, {}, {})>".format(
+            repr(self.u), 
+            repr(self.v), 
+            repr(self.rho)
+        )
+        
+
+def is_sorted(l, compare):
+    for l_1, l_2 in zip(l[:-1], l[1:]):
+        if not compare(l_1, l_2):
+            return False
+    return True
+
+
+class InvalidMaskInputException(Exception):
+    pass
+
+class NotMaskableTensorDegreeException(Exception):
+    pass
+
+class ObjectNotMaskableException(Exception):
+    pass
 
 
 class Mask:
-    def __init__(self, mask):
+    def __init__(self, M, mask):
+        if not is_sorted(mask, lambda l_1, l_2: l_1 < l_2):
+            raise InvalidMaskInputException()
+        self._M = M
         self._mask = mask
+        self._anti_mask = list([m for m in range(M)])
+        for m in self._mask[::-1]:
+            del self._anti_mask[m]
 
     def apply_mask(self, x):
-        """
-        This function may not need all the asserts.
-        Move to other functions? Remove?
-        """
-        dimensions = len(x.shape)
-        if dimensions == 1:
-            assert len(x) >= len(mask)
-            return self._apply_mask_on_vector(x)
-        elif dimensions == 2:
-            assert len(x) == len(x[0])
-            assert len(x) >= len(mask)
-            return self._apply_mask_on_matrix(x)
-        elif dimensions == 3:
-            assert len(x) == len(x[0])
-            assert len(x) == len(x[0][0])
-            assert len(x) >= len(mask)
-            return self._apply_mask_on_tensor(x)
+        if type(x) == list:
+            return self._apply_mask_on_sparse_matrix(x)
+        elif type(x) == np.ndarray:
+            dimensions = len(x.shape)
+            if dimensions == 1:
+                return self._apply_mask_on_vector(x)
+            elif dimensions == 2:
+                return self._apply_mask_on_matrix(x)
+            else:
+                raise NotMaskableTensorDegreeException()
+        else:
+            raise ObjectNotMaskableException()
 
     def _apply_mask_on_vector(self, v):
         return v[self._mask]
 
     def _apply_mask_on_matrix(self, A):
-        # TODO: will not work for sparse matrices
         return A[self._mask, :][:, self._mask]
 
-    def _apply_mask_on_tensor(self, T):
-        # TODO: will not work for sparse tensors
-        return T[self._mask, :, :][:, self._mask, :][:, :, self._mask]
+    def _apply_mask_on_sparse_matrix(self, elements):
+        for m in self._anti_mask:
+            print("")
+        return [
+            (i, j, value)
+            for i, j, value in elements
+            if i in self.mask and j in self.mask
+        ]
+        # This is very computationally expensive.
+        # How about instead sorting the elements, then going through the mask
+        # and checking excluded ones.
+        # What if the mask
 
 
-class TimeDerivative(object):
+class SpatialDerivative(object):
+    # TODO: Write out derivative expressions
     def __init__(
         self, N, mu, kappa, p, mask, edge_bc="DIRICHLET", interior_bc="NEUMANN"
     ):
@@ -113,11 +132,11 @@ class TimeDerivative(object):
         self.mask = mask
         self.edge_bc = edge_bc
         self.interior_bc = interior_bc
-        self.D_x = self.make_D_x
-        self.D_y = self.make_D_y
-        self.D_xy = self.make_D_xy
-        self.D_xx = self.make_D_xx
-        self.D_yy = self.make_D_yy
+        self.x = self.make_D_x
+        self.y = self.make_D_y
+        self.xy = self.make_D_xy
+        self.xx = self.make_D_xx
+        self.yy = self.make_D_yy
 
     def make_D_x(self,):
         pass
@@ -135,39 +154,77 @@ class TimeDerivative(object):
         pass
 
 
-class TimeDerivativeU(TimeDerivative):
+def test_require_that_triple_vector_class_works_as_expected():
+    u1 = np.array([1.0, 1.0, 1.0])
+    v1 = np.array([1.0, -1.0, 2.0])
+    rho1 = np.array([1.0, 2.0, 3.0])
+    x1 = TripleVector(u1, v1, rho1)
+    x2 = TripleVector(u1, v1, rho1)
+    assert type(x1 - x2) == TripleVector
+    assert type(x1 + x2) == TripleVector
+    assert type(x1 + (-2) * x2) == TripleVector
+    assert type(x1 + x2 * (-5)) == TripleVector
+    assert type(x1 + x2 / (-5)) == TripleVector
+    with pytest.raises(IllegalMultiplicationException):
+        assert x1 * x2
+    assert type(RK4(1.0, x1, lambda x: -x)) == TripleVector
+
+
+def test_require_that_anti_mask_works_as_expected():
+    with pytest.raises(InvalidMaskInputException):
+        mask_object = Mask(1, [1,0])
+    mask_list = [2,5,7,8]
+    M = 10
+    mask_object = Mask(M, mask_list)
+    assert mask_object._anti_mask == [0,1,3,4,6,9]
+    with pytest.raises(ObjectNotMaskableException):
+        mask_object.apply_mask("not valid maskable")
+    with pytest.raises(NotMaskableTensorDegreeException):
+        mask_object.apply_mask(np.array([[[1.0]]]))
+
+
+test_require_that_triple_vector_class_works_as_expected()
+test_require_that_anti_mask_works_as_expected()
+
+
+class TimeDerivativeU(SpatialDerivative):
+    """ 
+    D : (R^N, R^N, R^N)  -->  R^N
+              u, v, rho  |->  ∂u/∂t(u,v,rho) 
+    """
+
     def __init__(self, *args):
         super(TimeDerivativeU, self).__init__(*args)
 
     def __call__(self, triple_vector):
-        """ 
-        With this, the class becomes a mathematical function,
-        D: (R^N, R^N, R^N)  -->  R^N
-                 u, v, rho  |->  ∂u/∂t(u,v,rho) 
-        """
-        # TODO
-        return triple_vector.u
+        # TODO: Write the actual formula
+        return - triple_vector.u
 
 
-class TimeDerivativeV(TimeDerivative):
+class TimeDerivativeV(SpatialDerivative):
     def __init__(self, *args):
         super(TimeDerivativeV, self).__init__(*args)
 
     def __call__(self, triple_vector):
-        # TODO
-        return triple_vector.v
+        # TODO: Write the actual formula
+        return - triple_vector.v
 
 
-class TimeDerivativeRho(TimeDerivative):
+class TimeDerivativeRho(SpatialDerivative):
     def __init__(self, *args):
         super(TimeDerivativeRho, self).__init__(*args)
 
     def __call__(self, triple_vector):
-        # TODO
-        return triple_vector.rho
+        # TODO: Write the actual formula
+        return - triple_vector.rho
 
 
 class TimeDerivativeTriple:
+    """ 
+    D : (R^N, R^N, R^N)  -->  (R^N, R^N, R^N)
+            (u, v, rho)  |->  (∂u/∂t, ∂v/∂t, ∂rho/∂t)
+    """
+
     def __init__(self, dudt, dvdt, drdt):
         self.dudt = dudt
         self.dvdt = dvdt
@@ -179,3 +236,42 @@ class TimeDerivativeTriple:
             self.dvdt(triple_vector),
             self.drdt(triple_vector),
         )
+
+
+def solver_example(N, h, N_t, mu, kappa, x0, p, mask):
+    f = TimeDerivativeTriple(
+        TimeDerivativeU(
+            N, mu, kappa, p, mask, edge_bc="DIRICHLET", interior_bc="NEUMANN"
+        ),
+        TimeDerivativeV(
+            N, mu, kappa, p, mask, edge_bc="DIRICHLET", interior_bc="NEUMANN"
+        ),
+        TimeDerivativeRho(
+            N, mu, kappa, p, mask, edge_bc="NEUMANN", interior_bc="DIRICHLET"
+        ),
+    )
+    states = [x0]
+    for n_t in range(N_t):
+        states.append(RK4(h, states[-1], f))
+    return states
+
+
+if __name__ != "__main__":
+    N = 50
+    N_t = 10
+    h = 0.1
+    mask = Mask(list([i for i in range(N * N)]))
+
+    mu = 1.0
+    zeta = 1.0
+    kappa = zeta + (mu / 3)
+    p = 0  # TODO: Actually define it from function
+
+    u0 = mask.apply_mask(np.zeros(N * N))
+    v0 = mask.apply_mask(np.zeros(N * N))
+    rho0 = mask.apply_mask(copy.deepcopy(p))  # TODO: see p
+
+    x0 = TripleVector(u0, v0, rho0)
+
+    solution = solver_example(N, h, N_t, mu, kappa, x0, p, mask)
+
